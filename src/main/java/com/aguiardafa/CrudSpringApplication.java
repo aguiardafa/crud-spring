b0906fc4e92701cb80eca985c717c81313315092
aguiardafa <diego.fernandes.aguiar@gmail.com>
2022-01-07 17:01:11 -0300

    feat(cursos): implementa CommandLineRunner que alimenta H2database para teste da API

--- src/main/java/com/aguiardafa/CrudSpringApplication.java
+++ src/main/java/com/aguiardafa/CrudSpringApplication.java
@@ -1,7 +1,11 @@
 package com.aguiardafa;
 
+import com.aguiardafa.model.Curso;
+import com.aguiardafa.repository.CursoRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudSpringApplication {
@@ -10,4 +14,20 @@ public class CrudSpringApplication {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner initDatabase(CursoRepository cursoRepository){
+		return args -> {
+			cursoRepository.deleteAll();
+
+			Curso c = new Curso();
+			c.setNome("Angular");
+			c.setCategoria("Front-end");
+			cursoRepository.save(c);
+
+			Curso c2 = new Curso();
+			c2.setNome("Spring");
+			c2.setCategoria("Back-end");
+			cursoRepository.save(c2);
+		};
+	}
 }

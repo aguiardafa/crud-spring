8825117d0422842fc17003b774bad7a9d3e7af04
aguiardafa <diego.fernandes.aguiar@gmail.com>
2022-01-07 16:39:20 -0300

    feat(cursos): cria o Repositório de Curso utilizando JPA

---
+++ src/main/java/com/aguiardafa/repository/CursoRepository.java
@@ -0,0 +1,10 @@
+package com.aguiardafa.repository;
+
+import com.aguiardafa.model.Curso;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CursoRepository extends JpaRepository<Curso, Long> {
+
+}
